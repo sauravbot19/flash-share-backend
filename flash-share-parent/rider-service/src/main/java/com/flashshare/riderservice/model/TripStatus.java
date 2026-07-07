ec811773634fe3ba77459ddb3adbe18d3b0dea54
Saurav Tayade <90100594+sauravbot19@users.noreply.github.com>
2026-07-07 11:33:30 +0530

    Delete flash-share-parent directory

--- flash-share-parent/rider-service/src/main/java/com/flashshare/riderservice/model/TripStatus.java
+++
@@ -1,8 +0,0 @@
-package com.flashshare.riderservice.model;
-
-public enum TripStatus {
-    REQUESTED,
-    ACCEPTED,
-    COMPLETED,
-    CANCELLED
-}
--- flash-share-parent/rider-service/src/main/java/com/flashshare/riderservice/model/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.flashshare.riderservice.model;
-
-public enum UserRole {
-    ROLE_RIDER,
-    ROLE_DRIVER
-}
